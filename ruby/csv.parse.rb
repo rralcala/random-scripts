0b055dc1e1ae9d57424ca83de97ad0c6bec6474f
Roberto Rodriguez <rralcala@gmail.com>
2014-01-25 20:37:30 +0200

    Clean up

--- ruby/csv.parse.rb
+++ ruby/csv.parse.rb
@@ -1,10 +1,15 @@
+#!/bin/ruby
 require 'csv'
-require 'get_impaired_kernel'
 require 'date'
 
-csv_text = File.read('./select-diagnostics(2).csv')
+if ARGV[0].nil?
+	abort 'CSV filename required.'
+end
+
+csv_text = File.read(ARGV[0])
 csv = CSV.parse(csv_text)
 csv.each do |line|
+  unless line[1].nil?
     Date.parse line[1]
-
+  end
 end
