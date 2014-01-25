87b7afd55949c3ed4ccf818d8f2af31763541f25
Roberto Rodriguez <rralcala@gmail.com>
2014-01-25 20:47:42 +0200

    Clean up

--- php/test.php
+++ php/test.php
@@ -1,29 +1,20 @@
 <?php
-require_once("lib.php");
-
-$fp = fopen("ToStay.csv","r+");$c=0;
-$dups = 0;
-
-while(!feof($fp))
-{
-	$line = fgets($fp);
-	list ($child, $forget, $parent, $balance, $lastTx) = explode(",",$line);
-	if(trim($child) == trim($parent))
-	{
-
-		$roots[$child] = $balance;
-	}
-	if(!isset($tree[$parent]))
-		$tree[$parent] = Array();
-	$tree[$parent][$child] = $balance;
-}
-//echo "<map version=\"0.9.0\"><node  TEXT=\"New Mindmap\">";
-foreach($roots as $key=>$value)
-	print_child_simple($key, $tree);
-//echo "</node></map>";
- 
- 
-fclose($fp);
-
-
+	if(isset($argv[1]))
+	$handle = fopen($argv[1],"r+");
+	else
+		exit(0);
+	
+	$str = "";
+	
+	while(!feof($handle))
+		$str .= fgets($handle);
+	// Replaces a group of whitespaces with only one.
+	$str = preg_replace("/\s+/"," ",$str);
+	fclose($handle);
+	$handle = fopen($argv[1],"w");
+	fwrite($handle, $str);
+	fclose($handle);
+	
+	
+	
 ?>
