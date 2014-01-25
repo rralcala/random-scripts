dd4b06adfd963d715b9c460346092e830602943b
Roberto Rodriguez <rralcala@gmail.com>
2014-01-25 20:14:29 +0200

    Added var N

--- bash/ssh-multiple.sh
+++ bash/ssh-multiple.sh
@@ -1,11 +1,13 @@
 #/bin/bash
 
+N=8
+
 while read line           
 do          
 	if [ -n "$line" ]; then 
 	    	echo $line
 		(echo 1 >> threads.running; ssh $line $@ >> output-$line; tail -n +2 threads.running > threads.running) & 
-		while [ `cat threads.running | wc -l` -gt 4 ]; do
+		while [ `cat threads.running | wc -l` -gt $N ]; do
 			sleep 1
 		done
 	fi  
