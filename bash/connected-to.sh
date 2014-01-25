ceb510c27ac751c52335a19f6358f9fcad71f6c8
Roberto Rodriguez <rralcala@gmail.com>
2014-01-25 19:55:39 +0200

    Script to tell to which IPs I'm connected to.

---
+++ bash/connected-to.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+if [[ -z "$1" ]]
+	then
+	SELF=$(hostname -I | cut -d' ' -f 1)
+else
+	SELF=$1
+fi
+echo "Assuming I am: " $SELF
+netstat -apentu | grep "$SELF" | awk '{ print $5; }' | cut -d: -f 1 | grep -vE  "$SELF|0.0.0.0" | sort | uniq
