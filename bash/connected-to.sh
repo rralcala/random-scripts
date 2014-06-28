2115e83bd4c4c04c777c3165b3f40bdf20a39742
Roberto Rodriguez <rralcala@gmail.com>
2014-06-28 16:31:44 +0200

    more comments

--- bash/connected-to.sh
+++ bash/connected-to.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This more like a nestat use case
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
@@ -11,5 +12,6 @@ if [[ -z "$1" ]]
 else
 	SELF=$1
 fi
+
 echo "Assuming I am: " $SELF
 netstat -apentu | grep "$SELF" | awk '{ print $5; }' | cut -d: -f 1 | grep -vE  "$SELF|0.0.0.0" | sort | uniq
