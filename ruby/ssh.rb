4e63c8bf59d8db6441adc826effadcdc05e615ab
Rodriguez Alcala <rralcala@gmail.com>
2016-05-03 22:14:50 -0700

    Comments

--- ruby/ssh.rb
+++ ruby/ssh.rb
@@ -6,16 +6,19 @@ abort ('Command is required') if ARGV[2].nil?
 user = ARGV[1]
 command = ARGV[2]
 
+# Build a clean array of host names from a file
+# And later use the Array as a queue of commands
 f = File.open(ARGV[0], "r")
 hosts = f.readlines
+f.close
 hosts.each(&:strip!)
 
 results = []
+threads = []
 
-ts = []
 20.times do 
-  ts << Thread.new() do
-    while queue.size > 0
+  threads << Thread.new() do
+    while hosts.size > 0
        run = [hosts.pop]
        run[1] = `ssh #{user}@#{run[0]} #{command}`
        results << run
@@ -23,8 +26,7 @@ ts = []
   end
 end
 
-ts.each(&:join)
-
+# Wait for the threads before printing results.
+threads.each(&:join)
 pp results
-f.close
 
